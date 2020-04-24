0798dab2a6c290da7501428168db38fd0fd44d80
Azbayar2020 <azaa.magbtc@gmail.com>
2020-04-24 14:17:04 +0800

    toglogchiin eeljiig solidog

--- app.js
+++ app.js
@@ -23,5 +23,20 @@ function shooShid() {
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
+    if (diceNumber !== 1) {
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        roundScore = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        diceDom.style.display = "none";
+        if (activePlayer === 0) {
+            activePlayer = 1;
 
+        } else {
+            activePlayer = 0;
+        }
+    }
 }
