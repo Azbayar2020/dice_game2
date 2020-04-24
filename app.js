de19513cb508bc38fb01a8aa8c15cd81bd4dd56c
Azbayar2020 <azaa.magbtc@gmail.com>
2020-04-24 13:20:43 +0800

    Shoog shidehed zurag soligddog bolgoloo

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ var roundScore = 0;
 
 // Шооны аль ч талаараа буусныг хадгалах хувьсагч хэрэгтэй 1-6 гэсэн утга
 
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-// window.document.querySelector('#score-1').innerHTML = "<em>haha</em>";
-document.querySelector(".dice").style.display = "none";
-console.log("Шоо: " + dice);
+document.getElementById('score-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", shooShid);
+
+function shooShid() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+}
